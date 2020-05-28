b3e4584ceda68899819ea275dcbb079dd1716645
Sundeep <sundeep@Sundeeps-MacBook-Pro.local>
2020-05-28 11:55:48 +0530

    Started JavaScript and source codes for May 28

---
+++ JavaScript/Introduction to JavaScript/app.js
@@ -0,0 +1,59 @@
+// Developer based output
+// - console.log()
+// User based output
+// - document.write()
+// alert()
+
+document.write("Hola, amigos. ");
+// alert("hi there, I am a alert panel");
+
+console.log("I am a message which is shown only on browser dev panel");
+console.log("I am a message which is shown only on browser dev panel");
+
+// Variables
+// Declaration
+var name;
+// Initialise
+name = "Sundeep";
+console.log(name);
+// Reinitialse
+name = "Charan";
+console.log(name);
+
+// Easy method (Decl + Init)
+var jfksljfkdsjfksljfsdfljksd = "dsfjksdfsdkjfksdjflskdfjlksjfdr";
+
+// Data types
+// 1. String => "fjsdkfjsklfjsklfjskldjflkds"
+// 2. Number => 14 or 14.133, -1, -1.4444
+// 3. Booleans => true or false
+// 4. Arrays => ??????
+// 5. Object => ??????
+// 6. Null => empty (no memory specified)
+// 7. Undefined => empty (memory specified)
+
+var somenumber = 2432424242.2348237487892;
+var boolvar = true;
+
+var something;
+something = null;
+
+// Receiving input from user
+// var othername = prompt("Enter your name");
+var othername;
+console.log(othername);
+
+// Operators
+// Arithmetic
+  // +, -, /, *, %
+// Logical
+  // &&, ||
+// comparison
+  // Strictly equal (===)
+  // Normal equal (==)
+  // less than or equal (<=)
+  // Greater than or equal (>=)
+  // Less than, greater than (<, >)
+  // Not equal (normal !=)
+  // Not equal (strict !==)
+// Ternary => ???
